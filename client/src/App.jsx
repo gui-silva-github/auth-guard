5a5cf135fb5a29424dfe3e8990d42fe9fd9acfe3
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2024-12-21 11:06:11 -0300

    App do  AuthGuard

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import EmailVerify from './pages/EmailVerify'
+import Login from './pages/Login'
+import Home from './pages/Home'
+import ResetPassword from './pages/ResetPassword'
+import {ToastContainer, toast} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+const App = () => {
+  return (
+    <div>
+      <ToastContainer/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/email-verify" element={<EmailVerify/>}/>
+        <Route path="/reset-password" element={<ResetPassword/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
